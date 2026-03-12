154b7450eeb19f6caa93f893cdcf195f1d5fcef1
caic <calvin519120063@gmail.com>
2026-03-12 09:41:10 +0800

    主持人调度问题

---
+++ BM96.py
@@ -0,0 +1,57 @@
+# 描述 有 n 个活动即将举办，每个活动都有开始时间与活动的结束时间，第 i 个活动的开始时间是 starti ,第 i 个活动的结束时间是 endi ,
+# 举办某个活动就需要为该活动准备一个活动主持人。 一位活动主持人在同一时间只能参与一个活动。并且活动主持人需要全程参与活动，
+# 换句话说，一个主持人参与了第 i 个活动，
+# 那么该主持人在 (starti,endi) 这个时间段不能参与其他任何活动。求为了成功举办这 n 个活动，最少需要多少名主持人。
+
+
+from typing import List
+
+class Solution:
+    def minmumNumberOfHost(self , n: int, startEnd: List[List[int]]) -> int:
+        
+        starts = [x[0] for x in startEnd]
+        ends = [x[1] for x in startEnd]
+        
+        starts.sort() # O(n log n)
+        ends.sort() 
+        
+        i = j = 0
+        host = 0
+        res = 0
+    #      ---
+    #           ---
+    #               ----
+        while i < n:   # O(n)        # 一直盯着的是开始的活动
+            if starts[i] < ends[j]:  # 先开始的活动的开始时间 小于 先结束的活动的结束时间，不管是不是一个活动（这里主持人管的是某个时间段内的所有活动，而不是针对某一个）
+                host += 1
+                res = max(res, host) # 
+                i += 1
+            else:
+                host -= 1
+                j += 1
+                
+        return res
+    
+class Solution: 
+    def minmumNumberOfHost(self , n: int, startEnd: List[List[int]]) -> int: 
+        # write code here
+        # 同一时间内，同时有多少个活动同时进行
+        start = [e[0] for e in startEnd]
+        end = [e[1] for e in startEnd]
+        start.sort()
+        end.sort()
+        
+        i,j = 0,0
+        host_for_current_period = 0
+        host_max = 0
+        
+        while i < n: # 确保所有活动都考虑
+            # 一个时间段对应一个主持人，从 最早开始 和 最早结束 这个时间段开始
+            if start_sort[i] < end_sort[j]:        # 意味着当前时间段的活动还在进行，又开始了新的活动
+                host_for_current_period += 1
+                host_max = max(host_max,host_for_current_period)
+                i += 1
+            else:  # 开始以新起点开始的新的时间段的活动
+                host_for_current_period -= 1
+                j += 1
+        return host_max
